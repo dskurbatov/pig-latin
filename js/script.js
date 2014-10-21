a7c3b5de73d17e9f53912e8d124995a3c10af7d4
dskurbatov <dskurbatov13@gmail.com>
2014-10-21 15:38:44 -0700

    modified script.js file for pig-latin project. Now, it's working for sentences as well

--- js/script.js
+++ js/script.js
@@ -1,30 +1,39 @@
-var pigLatin = function (word) {
-	word = word.toLowerCase();
-    var vowels = /\b[aeiou]/;
-    var firstLetter = word.charAt(0);
-    var restOfWord = word.slice(1);
-    if (word.match(vowels)) {
-        return firstLetter + restOfWord + "ay";
-    } else {
-
-    	if((word.charAt(0) === "q") && (word.charAt(1) === "u")){
-        		return word.slice(2) + word.slice(0, 2) + "ay";
-        	};
-
-        if (!restOfWord.match(vowels)) { 
-
-        	if((restOfWord.charAt(0) === "q") && (restOfWord.charAt(1) === "u")){
-        		return restOfWord.slice(2) + firstLetter + restOfWord.slice(0, 2) + "ay";
-        	};	
-
-            if (!restOfWord.slice(1).match(vowels)) {
-                return restOfWord.slice(2) + firstLetter + restOfWord.charAt(0) + restOfWord.slice(1).charAt(0) + "ay";
-            } else {
-                return restOfWord.slice(1) + firstLetter + restOfWord.charAt(0) + "ay";
-            };
+var pigLatin = function (string) {
+    var cleanString = string.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    var words = cleanString.split(" ");
+    var result = "";
+    for (var i = 0; i < words.length; i++) {
+        var word = words[i];
+        word = word.toLowerCase();
+        var vowels = /\b[aeiou]/;
+        if (word.match(vowels)) {
+            result += word + "ay";
         } else {
-            return restOfWord + firstLetter + "ay";
-        }
 
+            if ((word.charAt(0) === "q") && (word.charAt(1) === "u")) {
+                result += word.slice(2) + word.slice(0, 2) + "ay";
+            } else {
+
+                if (!word.slice(1).match(vowels)) {
+
+                    if ((word.slice(1).charAt(0) === "q") && (word.slice(2).charAt(0) === "u")) {
+                        result += word.slice(3) + word.charAt(0) + word.slice(1, 3) + "ay";
+                        break;
+                    };
+
+                    if (!word.slice(2).match(vowels)) {
+                        result += word.slice(3) + word.charAt(0) + word.slice(1).charAt(0) + word.slice(2).charAt(0) + "ay";
+                    } else {
+                        result += word.slice(2) + word.charAt(0) + word.slice(1).charAt(0) + "ay";
+                    };
+
+                } else {
+                    result += word.slice(1) + word.charAt(0) + "ay";
+                }
+            }
+        };
     };
+    return result;
 };
+
+
